30d3f4630d448630a2122790fe1666716be1ae9f
Sr.Axado <48120843+SrAxado@users.noreply.github.com>
2020-07-16 12:47:36 +0100

    fixing up List display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <div>
       <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
       </Link>
-      <b>Amount:</b> {amount}<br />
-      <b>Created at:</b> {createdAt}<br />
-      <br /><br />
+      <p>
+        <b>Amount:</b> {numeral(amount).format('$0,0.00')}<br />
+        --
+        <b>Created at:</b> {moment(createdAt).format('MMMM Do, YYYY')}
+      </p>
     </div>
   );
 
